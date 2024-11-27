5477794a3b7f9c74aa4baaec789f3201efe8b674
luis-otavio-dias <luis777dias231@gmail.com>
2024-11-27 02:35:14 -0300

    att: criado o modulo styles e adiconado qdarkstyle

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ from PySide6.QtWidgets import QApplication
 from main_window import MainWindow, Info
 from variables import WINDOW_ICON_PATH
 from display import Display
+from styles import setupTheme
 
 if __name__ == "__main__":
     # Cria a aplicação
     app = QApplication(sys.argv)
+    setupTheme(app)
     window = MainWindow()
 
     # Cria o ícone
